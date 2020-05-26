91cad53a8135527ed15291fb8f56ccc3879822fe
Thomas <thomas.foch@outlook.fr>
2020-05-26 10:45:37 +0200

    Add sprite when initiation

--- SetUp.ino
+++ SetUp.ino
@@ -1,6 +1,12 @@
 void setup() {
   Serial.begin(9600);
+
+  // user interface switches
+  uiSwitches.begin();
+  
   BluetoothSerial.begin(9600);  // HC-05 9600 baud
+  
   P.begin();
-  P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+  P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, PA_SPRITE, PA_SPRITE);
+  P.setSpriteData(pacman1, W_PMAN1, F_PMAN1, pacman2,  W_PMAN2, F_PMAN2);
 }
